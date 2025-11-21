f59ccef677361b0e65e54c9d1ee877ff59340d7b
Braden Griebel <bgriebel@uw.edu>
2025-11-21 12:17:30 -0800

    Move to GPL-3.0-License
    Move to the GPL-3.0-License to comply with the license of the
    example file from river that this layout-generator was based on.

--- delta.c
+++ delta.c
@@ -1,8 +1,24 @@
 /*
  * Simple layout generator for River, with swapable layouts
  *
- * Based on example from:
+ * Based on example from, licensed under the GPL-3.0-only:
  * https://codeberg.org/river/river/src/branch/master/contrib/layout.c
+ *
+ *  This program is simmilarly licensed under the GPL-3.0-only
+ *  Copyright (C) 2025  Braden Griebel
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include <assert.h>
 #include <ctype.h>
